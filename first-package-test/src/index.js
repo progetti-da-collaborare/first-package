cf0cb5f0827e39c0763ca726d6ec75cdf90dba8a
Jaimlja <andreyev.vv@gmail.com>
2025-08-08 17:40:32 +0300

    works 1

--- first-package-test/src/index.js
+++ first-package-test/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Qwe from "@progetti-da-collaborare/try-simple-package"
+import Qwe from "@progetti-da-collaborare/first-package"
 
 //const y = qwe()
 
